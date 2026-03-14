355a4ef8a6bc7581a6c9efa39c788a88fc48d273
RYUJEONGHUN <ryugh0210@naver.com>
2026-03-14 22:39:46 +0900

    main.py

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from app.api import chat, data
+
+app = FastAPI(title="IncheonMate AI Server")
+
+# 라우터 연결 (prefix를 통해 URL을 구분합니다)
+app.include_router(data.router, prefix="/api/v1/data")
+app.include_router(chat.router, prefix="/api/v1/ai")
+
+@app.get("/")
+async def root():
+    return {"message": "IncheonMate FastAPI Server is running"}
