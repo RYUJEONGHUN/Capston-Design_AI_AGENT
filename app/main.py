16995e9bc927ca9ade2575a0432141eb391323f2
RYUJEONGHUN <ryugh0210@naver.com>
2026-05-10 03:22:12 +0900

    첫 cicd테스트

--- app/main.py
+++ app/main.py
@@ -10,8 +10,9 @@ app.include_router(chat.router, prefix="/api/v1/ai")
 app.include_router(chat_qwen_router, prefix="/api/v1/ai")
 app.include_router(chat_qwen_agent_router, prefix="/api/v1/ai")
 
+# 테스트
 @app.get("/")
 async def root():
-    return {"message": "IncheonMate FastAPI Server is running"}
+    return {"message": "IncheonMate FastAPI Server is running, test plz"}
 
 
