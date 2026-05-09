d8554e25553881bbb231770d59d088686da8f6fe
RYUJEONGHUN <ryugh0210@naver.com>
2026-05-10 01:26:31 +0900

    main 파일

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
-from app.api import chat, data
+from app.api import chat
+from app.api.chat_qwen import router as chat_qwen_router
+from app.api.qwen_experimental.chat_qwen_agent import router as chat_qwen_agent_router
 
 app = FastAPI(title="IncheonMate AI Server")
 
 # 라우터 연결 (prefix를 통해 URL을 구분합니다)
-app.include_router(data.router, prefix="/api/v1/data")
 app.include_router(chat.router, prefix="/api/v1/ai")
+app.include_router(chat_qwen_router, prefix="/api/v1/ai")
+app.include_router(chat_qwen_agent_router, prefix="/api/v1/ai")
 
 @app.get("/")
 async def root():
